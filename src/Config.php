cc6e56186886a8a659faabf9ddc03326bde9ce2d
xu42 <xu42.cn@gmail.com>
2017-02-06 16:45:18 +0800

    教务处新闻列表

---
+++ src/Config.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Xu42\DlpuNews;
+
+/**
+ * 配置文件
+ *
+ * 所有配置参数均在本文件进行定义
+ */
+
+class Config
+{
+    public static $config = [
+        'url_index'               => 'http://jiaowu.dlpu.edu.cn',         // 教务网站首页
+        'url_current_events'      => '/more/2/1',                         // 新闻动态
+        'url_notice'              => '/more/3/1',                         // 通知公告
+        'url_teaching_files'      => '/more/4/1',                         // 教务文件
+    ];
+
+}
---
+++ src/DlpuNews.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Xu42\DlpuNews;
+
+
+class DlpuNews extends ToolCrawl
+{
+    public function currentEvents()
+    {
+        $url = Config::$config['url_index'] . Config::$config['url_current_events'];
+        return $this->get($url);
+    }
+
+    public function notice()
+    {
+        $url = Config::$config['url_index'] . Config::$config['url_notice'];
+        return $this->get($url);
+    }
+
+    public function teachingFiles()
+    {
+        $url = Config::$config['url_index'] . Config::$config['url_teaching_files'];
+        return $this->get($url);
+    }
+
+    protected function get($url)
+    {
+        $content = $this->myCurl($url);
+        return $this->re($content);
+    }
+}
